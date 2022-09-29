fced4c6783f9e08bac2920e90d784fd0da71f92c
jan <jan3llam@gmail.com>
2022-09-29 14:39:22 +0300

    create db

--- test.py
+++ test.py
@@ -2,5 +2,17 @@ import requests
 
 BASE = "http://127.0.0.1:8000/"
 
-response = requests.put(BASE +"video/1",{"likes":10})
+data =[{"likes":10,"name":"janm","views": 1500},
+		{"likes":15,"name":"tim","views": 1100},
+		{"likes":222,"name":"edw","views": 2500}]
+
+for i in range(len(data)):
+	response = requests.put(BASE +"video/"+str(i),data[i])
+	print(response.json())
+
+input()
+response=requests.delete(BASE+"video/0")
+print(response)
+input()
+response = requests.get(BASE +"video/2")
 print(response.json())
